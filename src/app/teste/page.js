ebb23363154dc37d4f148614a0c4eedc39ddc4c5
Eduardo Ribeiro Silva <eduardoolive777@gmail.com>
2025-08-01 17:36:49 -0300

    Get the another api

--- src/app/teste/page.js
+++ src/app/teste/page.js
@@ -1,12 +1,38 @@
+const URL = 'https://dev.to/api/articles?tag=javascript';
 
-let a = 1
-let b = 2
+async function getNoticesDev() {
+    const response = await fetch(URL);
 
-export default function test (){
+    if (response.status === 200) {
+        const data = await response.json();
+        return data;
+    }
+
+    return []; // retorna lista vazia se der erro
+}
+
+export default async function Test() {
+    const data = await getNoticesDev();
+    //console.log(data)
+
     return (
-        <div>
-            {a}
-            {b}
+        <div className="min-h-screen bg-gray-100 p-4">
+
+            <nav className="bg-gray-200 p-6 rounded-lg shadow-md w-full max-w-2xl mx-auto my-4 ">
+                <h1 className="text-4xl font-bold text-blue-600">   Notícias de JavaScript do Dev.to :)</h1>
+            </nav>
+
+            <nav className="w-full">
+                <ul className="text-3xl font-bold text-blue-600">
+                    {data.map((item) => (
+                        <li key={item.id} className="bg-gray-200 p-6 rounded-lg shadow-md w-full max-w-5xl mx-auto my-4">
+                            <a>
+                                {item.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
